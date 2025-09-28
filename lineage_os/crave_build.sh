f4da9802b12d0717d91c94fa168869b595f04002
ICECOLD90191 <trade90191@gmail.com>
2025-09-28 17:06:02 +0530

    Update crave_build.sh

--- lineage_os/crave_build.sh
+++ lineage_os/crave_build.sh
@@ -20,7 +20,7 @@ rm -rf out/target/product/udon/vendor
 
 # Rom source repo
 echo -e "${GREEN}Initializing repo...${NC}"
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -64,7 +64,7 @@ echo "====== Envsetup Done ======="
 
 # Lunch
 echo -e "${GREEN}Running lunch command...${NC}"
-lunch lineage_udon-bp1a-userdebug
+lunch lineage_udon-ap3a-userdebug
 echo -e "${GREEN}Lunch completed successfully${NC}"
 
 # Install clean
